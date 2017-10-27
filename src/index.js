ffbc36e212e7cbbb5a8306fffcd7612088c50176
Leigh Barnes <leighbrendonbarnes@gmail.com>
2017-10-27 05:55:13 +0100

    Example Index ->

---
+++ src/index.js
@@ -0,0 +1,28 @@
+/**
+ * Module Info:
+ * Add what you want or clean it out I care little.
+ */
+
+/**
+ * DataSets or static imports?
+ * can be accessed using relative: `./static/`
+ * I guess common or ES6 Imports should work. Note V8 still does not include sup[port for ES6 imports.]
+ */
+
+// const testData = require('./static/test.json')
+
+
+
+/**
+ * Default Function? Example.
+ * Always a good I dea to tell other devs what you may be doing here, your call. Commenting is next to godliness.
+ * 
+ * Default Params. 
+ * @param {number} param1
+ * @param {bool} param2
+ * 
+ */
+
+// export default function(param1, param2) {
+// 	console.log(param1 + param2)
+// }
